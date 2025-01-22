6aa431a5cf78b50c56e42b46e1d6df8c9cae6e53
Aymen Alsuhaiqi <engaymen.work@gmail.com>
2025-01-22 15:33:49 +0300

    edit

--- dept/settings.py
+++ dept/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0mxt3!mto^%rkl##l!wby1_d^&)_i=+2*t3h&-vj))dn6gj0i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR/'static'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'dept/static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
